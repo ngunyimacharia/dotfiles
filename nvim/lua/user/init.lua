f33884d6cbceeb9b1cd436524c671a8f634f30a0
Kelvin Macharia Ngunyi <ngunyimacharia@gmail.com>
2022-07-05 13:25:48 +0200

    Color scheme changed to custom catppuccin

--- nvim/lua/user/init.lua
+++ nvim/lua/user/init.lua
@@ -18,7 +18,7 @@ local config = {
   },
 
   -- Set colorscheme
-  colorscheme = "default_theme",
+  colorscheme = "catppuccin",
 
   -- set vim options here (vim.<first_key>.<second_key> =  value)
   options = {
@@ -90,7 +90,84 @@ local config = {
       {
         's1n7ax/nvim-search-and-replace',
         config = function() require'nvim-search-and-replace'.setup() end,
-      }
+      },
+      {
+        "catppuccin/nvim",
+        as = "catppuccin",
+        config = function()
+          require("catppuccin").setup {
+            transparent_background = false,
+            term_colors = false,
+            styles = {
+	            comments = "italic",
+	            conditionals = "italic",
+	            loops = "NONE",
+	            functions = "NONE",
+	            keywords = "NONE",
+	            strings = "NONE",
+	            variables = "NONE",
+	            numbers = "NONE",
+	            booleans = "NONE",
+	            properties = "NONE",
+	            types = "NONE",
+	            operators = "NONE",
+            },
+            integrations = {
+	            treesitter = true,
+	            native_lsp = {
+		            enabled = true,
+		            virtual_text = {
+			            errors = "italic",
+			            hints = "italic",
+			            warnings = "italic",
+			            information = "italic",
+		            },
+		            underlines = {
+			            errors = "underline",
+			            hints = "underline",
+			            warnings = "underline",
+			            information = "underline",
+		            },
+	            },
+                coc_nvim = false,
+	            lsp_trouble = false,
+	            cmp = true,
+	            lsp_saga = false,
+	            gitgutter = false,
+	            gitsigns = true,
+	            telescope = true,
+	            nvimtree = {
+		            enabled = true,
+		            show_root = false,
+		            transparent_panel = false,
+	            },
+	            neotree = {
+		            enabled = false,
+		            show_root = false,
+		            transparent_panel = false,
+	            },
+	            which_key = false,
+	            indent_blankline = {
+		            enabled = true,
+		            colored_indent_levels = false,
+	            },
+	            dashboard = true,
+	            neogit = false,
+	            vim_sneak = false,
+	            fern = false,
+	            barbar = false,
+	            bufferline = true,
+	            markdown = true,
+	            lightspeed = false,
+	            ts_rainbow = false,
+	            hop = false,
+	            notify = true,
+	            telekasten = true,
+	            symbols_outline = true,
+            }
+          }
+        end,
+      },
     },
     -- All other entries override the setup() call for default plugins
     ["null-ls"] = function(config)
