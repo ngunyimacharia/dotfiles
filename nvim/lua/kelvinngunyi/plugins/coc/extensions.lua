124b23b110d9ab9abfc0e6cecb6051dd43de3325
Kelvin Macharia Ngunyi <ngunyimacharia@gmail.com>
2023-11-07 14:58:45 +0300

    feat: manage coc extensions in lua file

---
+++ nvim/lua/kelvinngunyi/plugins/coc/extensions.lua
@@ -0,0 +1,12 @@
+vim.g.coc_global_extensions = {
+'coc-css',
+'coc-highlight',
+'coc-html',
+'coc-json',
+'coc-prettier',
+'coc-tsserver',
+'coc-sumneko-lua',
+'@yaegassy/coc-intelephense',
+'coc-php-cs-fixer',
+}
+
