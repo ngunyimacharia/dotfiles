5c0d75382d9458e2d933b592a038b03aeefaff53
Kelvin Macharia Ngunyi <ngunyimacharia@gmail.com>
2023-11-07 15:54:45 +0300

    fix: remove duplicate line

--- nvim/lua/kelvinngunyi/core/options.lua
+++ nvim/lua/kelvinngunyi/core/options.lua
@@ -36,6 +36,3 @@ opt.splitright = true
 opt.splitbelow = true
 
 opt.iskeyword:append("-")
-
--- configure coc to use node 16
-vim.g.coc_node_path = '/Users/kelvinngunyi/.nvm/versions/node/v16.20.1/bin/node'
