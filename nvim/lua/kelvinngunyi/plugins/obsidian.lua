2889db25d2d14d205c81dd9df279f9dcc829ab5c
Kelvin Macharia Ngunyi <ngunyimacharia@gmail.com>
2024-01-24 14:46:36 +0300

    feat: obsidian new note creation customised

--- nvim/lua/kelvinngunyi/plugins/obsidian.lua
+++ nvim/lua/kelvinngunyi/plugins/obsidian.lua
@@ -32,4 +32,22 @@ obsidian.setup({
     -- A map for custom variables, the key should be the variable and the value a function
     substitutions = {}
   },
+
+  -- Optional, customize how names/IDs for new notes are created.
+  note_id_func = function(title)
+    -- A note with the title 'My new note' will given an ID that looks
+    -- like 'my-new-note', and therefore the file name 'my-new-note.md'
+    if title ~= nil then
+      -- If title is given, transform it into valid file name.
+      return title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+    else
+      -- If title is nil, just add 4 random uppercase letters to the suffix.
+      local suffix = ""
+      for _ = 1, 4 do
+        suffix = suffix .. string.char(math.random(65, 90))
+      end
+      return tostring(os.time()) .. "-" .. suffix
+    end
+  end,
+
 })
