b46a1ef65980021ffa2916813e8ec86158c61f24
Kelvin Macharia Ngunyi <ngunyimacharia@gmail.com>
2024-02-19 21:54:51 +0300

    fix: lazygit setup

--- nvim/lua/kelvinngunyi/plugins/lazygit.lua
+++ nvim/lua/kelvinngunyi/plugins/lazygit.lua
@@ -1,6 +1,7 @@
 -- import nvim-lazygit safely
-local lazygit_setup, lazygit = pcall(require, "nvim-lazygit")
+local lazygit_setup, lazygit = pcall(require, "lazygit.nvim")
 if not lazygit_setup then
+  print("Lazygit not found")
   return
 end
 
