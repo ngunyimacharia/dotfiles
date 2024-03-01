d9ae3e567ff470d9f137f4ae8005d1cdf7d0d679
Kelvin Macharia Ngunyi <ngunyimacharia@gmail.com>
2024-03-01 14:26:08 +0300

    feat: add theme changing

---
+++ kitty/change_kitty_theme.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+DARK_MODE=$(2>/dev/null defaults read -g AppleInterfaceStyle)
+
+# Set the Kitty config directory
+KITTY_CONFIG_DIR="${HOME}/.config/kitty/kitty-themes/themes"
+
+# Define the light and dark theme names
+LIGHT_THEME="AtomOneLight.conf"
+DARK_THEME="Atom.conf"
+
+# Define the theme to use based on the value of DARKMODE
+if [ -n "$DARK_MODE" ] && [ "$DARK_MODE" = "Dark" ]; then
+	THEME="${DARK_THEME}"
+else
+	THEME="${LIGHT_THEME}"
+fi
+
+# Check if the theme file exists
+if [[ -f "${KITTY_CONFIG_DIR}/${THEME}" ]]; then
+	# Use kitty command to set colors
+	kitty @ set-colors -a "${KITTY_CONFIG_DIR}/${THEME}"
+	# echo "Theme successfully set to ${KITTY_CONFIG_DIR}/${THEME}"
+else
+	# echo "Error: ${KITTY_CONFIG_DIR}/${THEME} theme file not found"
+	exit 1
+fi
