fb9c1b7b3ed7af96e47d51b2ac3475ca2ad6c6a6
Kelvin Macharia Ngunyi <ngunyimacharia@gmail.com>
2024-03-05 15:32:13 +0300

    feat: add php-cs-fixer formatter

--- lazyvim/lua/plugins/conform.lua
+++ lazyvim/lua/plugins/conform.lua
@@ -3,6 +3,7 @@ return {
     "stevearc/conform.nvim",
     opts = {
       formatters_by_ft = {
+        php = { "php_cs_fixer", "intelephense" },
         blade = { "prettier" },
         fish = {},
       },
