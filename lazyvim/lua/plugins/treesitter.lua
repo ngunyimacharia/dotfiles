cf87640c3af0b85420d3d46f39e26458857d45ef
Kelvin Macharia Ngunyi <ngunyimacharia@gmail.com>
2024-03-07 15:12:07 +0300

    feat: add blade support for treesitter

--- lazyvim/lua/plugins/treesitter.lua
+++ lazyvim/lua/plugins/treesitter.lua
@@ -1,14 +1,23 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {
-        "php",
-        "go",
-        "gomod",
-        "gowork",
-        "gosum",
+  "nvim-treesitter/nvim-treesitter",
+  opts = function(_, opts)
+    require("nvim-treesitter.parsers").get_parser_configs().blade = {
+      install_info = {
+        url = "https://github.com/EmranMR/tree-sitter-blade",
+        files = { "src/parser.c" },
+        branch = "main",
       },
-    },
-  },
+      filetype = "blade",
+    }
+
+    opts.ensure_installed = opts.ensure_installed or {}
+    vim.list_extend(opts.ensure_installed, {
+      "php",
+      "go",
+      "gomod",
+      "gowork",
+      "gosum",
+      "blade",
+    })
+  end,
 }
