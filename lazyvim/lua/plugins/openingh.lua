29abf29f85a24208b757db3d8da4d274eeff4eaa
Kelvin Macharia Ngunyi <ngunyimacharia@gmail.com>
2024-03-07 15:20:11 +0300

    fix: keybind

--- lazyvim/lua/plugins/openingh.lua
+++ lazyvim/lua/plugins/openingh.lua
@@ -2,7 +2,7 @@ return {
   {
     "almo7aya/openingh.nvim",
     keys = {
-      { "<leader>gr", "<cmd>OpenInGHRepo<cr>", desc = "Open project's Git repository" },
+      { "<leader>go", "<cmd>OpenInGHRepo<cr>", desc = "Open project's Git repository" },
       { "<leader>gf", "<cmd>OpenInGHFile<cr>", desc = "Open current file in Git repository" },
       { "<leader>gl", "<cmd>OpenInGHFileLines<cr>", desc = "Open current line in Git repository" },
     },
