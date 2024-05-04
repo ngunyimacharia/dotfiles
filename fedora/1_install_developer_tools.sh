9a1dcf29141123d7026ec8bae7606547e2b7097e
Kelvin Macharia Ngunyi <ngunyimacharia@gmail.com>
2024-05-05 02:32:11 +0300

    feat: install oh my zsh

--- fedora/1_install_developer_tools.sh
+++ fedora/1_install_developer_tools.sh
@@ -1,3 +1,7 @@
+# Install ohmyzsh
+sudo dnf install zsh -y
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 # Install Kitty
 sudo dnf install kitty -y
 
