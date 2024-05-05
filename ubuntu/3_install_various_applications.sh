8e3dd6f6799105ca2f1f7d6b994013c79ee853a4
Kelvin Macharia Ngunyi <ngunyimacharia@gmail.com>
2024-05-06 00:17:40 +0300

    feat: install slack

--- ubuntu/3_install_various_applications.sh
+++ ubuntu/3_install_various_applications.sh
@@ -3,3 +3,6 @@ wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo gpg --dear
 sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/chrome-keyring.gpg] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google.list'
 sudo apt update
 sudo apt install google-chrome-stable -y
+
+# Install Slack
+flatpak install flathub com.slack.Slack -y
