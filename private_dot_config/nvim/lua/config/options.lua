7af58fea697aa0e91eca1dcb3312cce640455be2
Kelvin Ngunyi <ngunyimacharia@gmail.com>
2024-11-10 18:39:12 +0300

    feat: install avante to neovim

--- private_dot_config/nvim/lua/config/options.lua
+++ private_dot_config/nvim/lua/config/options.lua
@@ -1,3 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Added due to avante.nvim
+--views can only be fully collapsed with the global statusline
+vim.opt.laststatus = 3
