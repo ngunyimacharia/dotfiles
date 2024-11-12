c02e46b5d027e3371fd34a1ef4d87eb37f04b3fd
Kelvin Ngunyi <ngunyimacharia@gmail.com>
2024-11-12 09:08:48 +0300

    feat: openingh for nvim

---
+++ private_dot_config/nvim/lua/plugins/openingh.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "almo7aya/openingh.nvim",
+    keys = {
+      { "<leader>go", "<cmd>OpenInGHRepo<cr>", desc = "Open project's Git repository" },
+      { "<leader>gf", "<cmd>OpenInGHFile<cr>", desc = "Open current file in Git repository" },
+      { "<leader>gl", "<cmd>OpenInGHFileLines<cr>", desc = "Open current line in Git repository" },
+    },
+  },
+}
