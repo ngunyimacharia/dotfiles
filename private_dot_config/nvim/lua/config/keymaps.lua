0e52a137253a3e7f9ca5c7b787b87b52c3e24e76
Kelvin Ngunyi <ngunyimacharia@gmail.com>
2024-11-12 20:11:46 +0300

    feat: install freeze and freeze.nvim

--- private_dot_config/nvim/lua/config/keymaps.lua
+++ private_dot_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Map Ctrl + s to save in normal mode
+vim.api.nvim_set_keymap("n", "<C-s>", ":w<CR>", { noremap = true, silent = true })
+-- Map Ctrl + s to save in insert mode
+vim.api.nvim_set_keymap("i", "<C-s>", "<Esc>:w<CR>a", { noremap = true, silent = true })
+
+-- Freeze
+vim.api.nvim_set_keymap("v", "<leader>sc", "<cmd>Freeze<cr>", {})
+vim.api.nvim_set_keymap("n", "<leader>so", "<cmd>Freeze open<cr>", {})
