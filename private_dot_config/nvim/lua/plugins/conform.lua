c3aae9bc1b0ad02a195470afe815e00fcce768bb
Kelvin Ngunyi <ngunyimacharia@gmail.com>
2024-11-12 16:59:46 +0300

    fix: conditionally use pint vs php_cs_fixer

--- private_dot_config/nvim/lua/plugins/conform.lua
+++ private_dot_config/nvim/lua/plugins/conform.lua
@@ -7,6 +7,20 @@ return {
         blade = { "blade-formatter" },
         fish = {},
       },
+      formatters = {
+        pint = {
+          condition = function(self, ctx)
+            local pint_config = "pint.json"
+            return vim.fn.filereadable(vim.fn.getcwd() .. "/" .. pint_config) == 1
+          end,
+        },
+        php_cs_fixer = {
+          condition = function(self, ctx)
+            local php_cs_config = ".php-cs-fixer.php"
+            return vim.fn.filereadable(vim.fn.getcwd() .. "/" .. php_cs_config) == 1
+          end,
+        },
+      },
     },
   },
 }
