19c07c4cae4dd27c31cfc4bcfbc9d96233de29d2
Kelvin Ngunyi <ngunyimacharia@gmail.com>
2025-07-09 22:24:53 +0300

    feat: disable mouse usage in nvim

--- private_dot_config/nvim/lua/config/options.lua
+++ private_dot_config/nvim/lua/config/options.lua
@@ -6,3 +6,13 @@
 --views can only be fully collapsed with the global statusline
 vim.opt.laststatus = 3
 vim.g.lazyvim_prettier_needs_config = false
+
+-- Disable cursor completely
+vim.keymap.set("", "<up>", "<nop>", { noremap = true })
+vim.keymap.set("", "<down>", "<nop>", { noremap = true })
+vim.keymap.set("i", "<up>", "<nop>", { noremap = true })
+vim.keymap.set("i", "<down>", "<nop>", { noremap = true })
+
+vim.opt.guicursor = ""
+vim.opt.mouse = ""
+vim.opt.mousescroll = "ver:0,hor:0"
